88de3c3ed13bfeb824dffe58922556e51e099f42
João Pedro <carvalho.jp99@gmail.com>
2023-05-08 13:05:16 -0300

    Código otimizado

--- determinar_sonda.py
+++ determinar_sonda.py
@@ -18,42 +18,30 @@ stdout, stderr = clustalw_cline()
 alinhamento = AlignIO.read("temp.aln", "clustal")
 
 from fuzzywuzzy import fuzz
-#import argparse
-#import PySimpleGui
+#import argparse || estudar esta biblioteca
+#import PySimpleGui || estudar esta biblioteca
 
 seq1 = alinhamento[0].seq
 seq2 = alinhamento[1].seq
 regioes_divergentes = []
 similaridade_sonda = float(input('similaridade da sonda: '))
 
+regioes_conservadas = []
 for i in range(0, len(seq1), 30):
     sub_lista1 = "".join(seq1[i:i+30])
     sub_lista2 = "".join(seq2[i:i+30])
     similaridade_seq = fuzz.ratio(sub_lista1, sub_lista2) #retorna a similaridade entre as duas listas pulando de 2 em 2
-    if similaridade_seq < similaridade_sonda:
+    if similaridade_seq == 100:
+        regioes_conservadas.append(sub_lista2)
+    elif similaridade_seq <= similaridade_sonda:
         regioes_divergentes.append([sub_lista2, similaridade_seq])
 
-nucleotideos_conservados = []
+# Depois tentar i:i+100, por exemplo, para desenhar os primers...
+# Ou continuar encontrando as regiões de 20 em 20 para depois expandir elas.
 
-for var1, var2 in zip(seq1, seq2):
+print(regioes_conservadas) 
 
-    posicao_var1 = seq1.index(var1)
-    posicao_var2 = seq2.index(var2)
-
-    if var1 == var2 and ((seq1[posicao_var1:posicao_var1+20]) == (seq2[posicao_var2:posicao_var2+20])):
-        nucleotideos_conservados.append(var1)
-
-regioes_conservadas = []
-
-for i in range(0, len(seq2), 20):
-    if str(seq2[i:i+20]) in regioes_divergentes:
-        continue    
-    regioes_conservadas.append(str(seq2[i:i+20])) # Depois tentar i:i+100, por exemplo, para desenhar os primers...
-    # Ou continuar encontrando as regiões de 20 em 20 para depois expandir elas.
-
-# print(regioes_conservadas) 
-
-print('----------------------------------------\n----------------------------------------\n----------------------------------------\n----------------------------------------\n')
+print('\n----------------------------------------\n----------------------------------------\n----------------------------------------\n----------------------------------------\n')
 
 print('Sondas: ')
 for j in regioes_divergentes:
